d75b50251a33f3b4db4837b3e2d3927a9d8eecee
Priya141092 <priyasaxena1492@gmail.com>
2021-03-09 15:34:11 +0530

    # WARNING: head commit changed in the meantime
    ccxx

--- Collections_Framework/src/HashMap/HashMapCompare.java
+++ Collections_Framework/src/HashMap/HashMapCompare.java
@@ -1,9 +1,13 @@
 package HashMap;
 
+import java.util.HashMap;
+
 public class HashMapCompare {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		HashMap<Integer, String> map1 = new HashMap<Integer, String>();
 
 	}
 
