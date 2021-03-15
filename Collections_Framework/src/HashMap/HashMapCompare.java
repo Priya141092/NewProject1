06afd75ca866f74a8e3a5e15b4de4e4a7e54b630
Priya141092 <priyasaxena1492@gmail.com>
2021-03-15 14:05:55 +0530

    yes commit

--- Collections_Framework/src/HashMap/HashMapCompare.java
+++ Collections_Framework/src/HashMap/HashMapCompare.java
@@ -1,6 +1,8 @@
 package HashMap;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class HashMapCompare {
 
@@ -8,7 +10,73 @@ public class HashMapCompare {
 		// TODO Auto-generated method stub
 		
 		HashMap<Integer, String> map1 = new HashMap<Integer, String>();
-		HashMap<Integer, String> map3 = new HashMap<Integer, String>();
+		
+		map1.put(1, "A");
+		map1.put(2, "B");
+		map1.put(3, "C");
+		
+		
+	   HashMap<Integer, String> map2 = new HashMap<Integer, String>();
+	   map2.put(3,"C");
+	   map2.put(1,"A");
+	   map2.put(2,"B");
+	   
+	   HashMap<Integer, String> map3 = new HashMap<Integer, String>();
+	   map3.put(1,"A");
+	   map3.put(2,"B");
+	   map3.put(3,"C");
+	   map3.put(3,"C");
+	   
+	   //.On te basi sof key vaalue pairs
+	   
+	   System.out.println(map1.equals(map2));
+	   
+	   
+	   //Compare 2 hashmaps with the same key
+	   
+	   System.out.println(map1.keySet().equals(map2.keySet()));//true
+	   System.out.println(map1.keySet().equals(map3.keySet()));//true
+	   
+	   //find out the extra key
+	   HashMap<Integer, String> map4 = new HashMap<Integer, String>();
+	   map4.put(1,"A");
+	   map4.put(2,"B");
+	   map4.put(3,"C");
+	   map4.put(4,"D");
+	   
+	   //combine keys from boths maps using Hashset
+	   HashSet<Integer> combinekeys = new HashSet<Integer>(map1.keySet());
+	   combinekeys.addAll(map4.keySet());
+	   combinekeys.removeAll(map1.keySet());
+	   System.out.println(combinekeys);
+	   
+	   //Compare maps by values
+	   HashMap<Integer, String> map5 = new HashMap<Integer, String>();
+		
+	   map5.put(1, "A");
+	   map5.put(2, "B");
+	   map5.put(3, "C");
+		
+		
+	   HashMap<Integer, String> map6 = new HashMap<Integer, String>();
+	   map6.put(4,"A");
+	   map6.put(5,"B");
+	   map6.put(6,"C");
+	   
+	   HashMap<Integer, String> map7 = new HashMap<Integer, String>();
+	   map7.put(1,"A");
+	   map7.put(2,"B");
+	   map7.put(3,"C");
+	   map7.put(4,"C");
+	   
+	   //duplicates are not allowed
+	   System.out.println(new ArrayList<>(map5.values()).equals(new ArrayList<>(map6.values())));
+	   
+	   //duplicate are allowed
+	   
+	   //use hashset
+	   System.out.println(new HashSet<>(map5.values()).equals(new HashSet<>(map7.values())));
+	   
 
 	}
 
---
+++ Interview_Questions/src/Java_quests/Strin_related_best_exampke.java
@@ -0,0 +1,17 @@
+package Java_quests;
+
+public class Strin_related_best_exampke {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  StringBuffer sb1 = new StringBuffer("Amit");
+		  StringBuffer sb2 = new StringBuffer("Amit");
+		  String ss1 = "Amit";
+		  System.out.println(sb1==sb2);
+		  System.out.println(sb1.equals(sb2));
+		  System.out.println(sb1.equals(ss1));
+		  System.out.println("Poddar".substring(3)); 
+
+	}
+
+}
